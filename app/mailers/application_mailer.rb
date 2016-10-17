b87b82915675312b0d60f1bcd0b9a699a8e3fc84
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-17 16:17:27 +0200

    change email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'freedive.cebu30@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,20 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.registration_confirmation.subject
+  #
+  def registration_confirmation
+    @greeting = "Hi"
+
+    mail to: "jyouji1979@gmail.com"
+    # mail to: "joji.urano@bibo.com.ph"
+  end
+
+  def notify_teacher(teacher)
+    @teacher = teacher
+    email = "jyouji1979@gmail.com"
+    mail to:  email, subject: "#{@teacher.name}が空いています"
+  end
+end
