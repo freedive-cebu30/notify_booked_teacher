2cf7b4e0d384941fb4907ba531c0f1f560a03033
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-17 16:50:46 +0200

    add production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'aqueous-spire-22637.herokuapp.com', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'freedive.cebu30@gmail.com',
+    :password => 'locahpkfqiroiytn',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
