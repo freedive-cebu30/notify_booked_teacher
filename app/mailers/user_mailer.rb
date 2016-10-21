75d016914c71e1aef178b09dd0f33072b69866a8
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-21 17:16:03 +0200

    fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,7 +14,7 @@ class UserMailer < ApplicationMailer
 
   def notify_teacher(teacher, user, params = {})
     @teacher = teacher
-    @lesson_number = params[:lesson_number].present? params[:lesson_number] : nil
+    @lesson_number = params[:lesson_number].present? ? params[:lesson_number] : nil
     @landing_page = params[:landing_page].present? ? params[:landing_page] : nil
     @m_landing_page =  params[:m_landing_page].present? ? params[:landing_page] : nil
 
