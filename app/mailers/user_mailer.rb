9609169843e3f43d46142ba6876a75d460385ada
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-22 15:02:12 +0200

    fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
     @teacher = teacher
     @lesson_number = params[:lesson_number].present? ? params[:lesson_number] : nil
     @landing_page = params[:landing_page].present? ? params[:landing_page] : nil
-    @m_landing_page =  params[:m_landing_page].present? ? params[:landing_page] : nil
+    @m_landing_page =  params[:m_landing_page].present? ? params[:m_landing_page] : nil
 
     email = user.email
     mail to:  email, subject: "#{@teacher.name}が#{@lesson_number}スロット空いています"
