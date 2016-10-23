f560c099cd18d5731cb862c46e036ff2f2320d87
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-23 12:40:25 +0200

    add google ana and service explanation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :teachers
+  get 'introductions', to: 'introductions#index'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   root to: 'teachers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
