059c0eb1d3566322af35ecaf56a1a134889563e9
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-10-29 10:28:07 +0200

    fix bug

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,7 +1,10 @@
 module TeachersHelper
   def display_active_li(current_action_name, target_action_name)
     tag =
-      if controller.controller_name == 'sessions' || 'passwords' || 'registrations' || 'confirmations'
+      if ( controller.controller_name == 'sessions' ) ||
+         ( controller.controller_name == 'passwords' ) ||
+         ( controller.controller_name == 'registrations' ) ||
+         ( controller.controller_name ==  'confirmations' )
         '<li>'
       elsif current_action_name == target_action_name
         '<li class="active">'
