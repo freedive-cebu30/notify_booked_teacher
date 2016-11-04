b1347cc0a4ee449b0bd87ba4093fdbfd57eb8517
freedive.cebu30 <freedive.cebu30@gmail.com>
2016-11-04 16:26:21 +0100

    change top

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :teachers
   get 'introductions', to: 'introductions#index'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  root to: 'teachers#index'
+  root to: 'introductions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
