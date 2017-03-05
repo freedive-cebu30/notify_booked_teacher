f1f215d257cc69d80f8220088db21eb42d66b8a6
freedive.cebu30 <freedive.cebu30@gmail.com>
2017-03-05 12:12:48 +0100

    add routes for teacher

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   resources :teachers
   get 'introductions', to: 'introductions#index'
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login',
+                                                                                  sign_out: 'logout',
+                                                                                  password: 'secret',
+                                                                                  confirmation: 'verification',
+                                                                                  unlock: 'unblock',
+                                                                                  registration: 'register',
+                                                                                  sign_up: 'cmon_let_me_in'
+                                                                                }
   root to: 'introductions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :users
+    resources :teachers, only: [:index]
   end
 end
