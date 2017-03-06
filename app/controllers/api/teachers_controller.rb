371f7b3cc690a3e6328a71c59445576e57cd338e
freedive.cebu30 <freedive.cebu30@gmail.com>
2017-03-06 16:42:48 +0100

    skip auth filter from api and add sample data

--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -1,7 +1,9 @@
 module API
   class TeachersController < ApplicationController
+    skip_before_filter :authenticate_user!
     def index
-      @teachers = current_user.teachers
+      # @teachers = current_user.teachers
+      @teachers = Teacher.all
     end
   end
 end
